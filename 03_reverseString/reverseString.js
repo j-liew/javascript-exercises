4dbd4483c415ee1d1f0b1910e940d0c2577dd74c
Justin Liew <justinwcliew@gmail.com>
2023-12-29 18:38:03 +0700

    complete first 4 exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    // output = "";
+    // for(let i = word.length - 1; i >= 0; i--) {
+    //     output += word[i];
+    // }
+    // return output;
+    return word.split("").reverse().join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,30 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elementsToRemove) {
+    // my original solution, way less efficient
 
-};
+    // for (elem of elementsToRemove) {
+    //     for (let i = 0; i < arr.length; i++){
+    //         if (arr[i] === elem) {
+    //             arr.splice(i, 1);
+    //         }
+    //     }
+    // }
+
+    // const outputArray = []
+    // arr.forEach((elem) => {
+    //     if (!elementsToRemove.includes(elem)) {
+    //         outputArray.push(elem);
+    //     }
+    // })
+
+    // return outputArray;
+
+    // simpler solution
+    return arr.filter((x) => {
+        return !elementsToRemove.includes(x);
+    })
+
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
