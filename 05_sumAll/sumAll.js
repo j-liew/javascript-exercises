8193bbb36b57cc90e9c3c254595994686cc8217e
Justin Liew <justinwcliew@gmail.com>
2024-01-02 23:49:12 +0700

    complete up to exercise 7 in javascript exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    // error checking
+    if (start < 0 || end < 0 || typeof start != 'number' || typeof end != 'number') {
+        return "ERROR";
+    }
 
+    // swap if larger number given as start
+    if (end < start) {
+        temp = start;
+        start = end;
+        end = temp;
+    }
+
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  // using toFixed
+  // return +((fahrenheit - 32) * (5 / 9)).toFixed(1);
+  // using Math.round()
+  return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+
+
+const convertToFahrenheit = function(celsius) {
+  // using toFixed
+  // toFixed returns a string, so must convert back to a number
+  // return +(celsius * (9/5) + 32).toFixed(1);
+  // using Math.round()
+  return Math.round((celsius * (9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
